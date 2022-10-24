5ce85df04b767b08394ffb24a8e8b413c97ddaed
Ellen Woodward <S00222321@atu.ie>
2022-10-24 10:37:05 +0100

    Add project files.

---
+++ Q1/Program.cs
@@ -0,0 +1,27 @@
+﻿using Q1;
+
+Team SligoRovers = new Team("Sligo Rovers");
+Team FinnHarps = new Team("Finn Harps");
+Team GalwayUnited = new Team("Galway United");
+Team DerryCity = new Team("Derry City");
+Team Dundalk = new Team("Dundalk FC");
+
+List<Team> teams = new List<Team>();
+
+teams.Add(SligoRovers);
+teams.Add(FinnHarps);
+teams.Add(GalwayUnited);
+teams.Add(DerryCity);
+teams.Add(Dundalk);
+
+DisplayTeams(teams);
+
+void DisplayTeams(List<Team> teams)
+{
+    Console.WriteLine($"Name\t\tPoints\tWins\tDraws\tLosses\tPlayed\t");
+
+    foreach (Team team in teams)
+    {
+        Console.WriteLine(team.DisplayTeamTable());
+    }
+}
---
+++ Q1/Team.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1
+{
+    internal class Team
+    {
+        public string TeamName { get; private set; }
+        public int Wins { get; private set; }
+        public int Draws { get; private set; }
+        public int Losses { get; private set; }
+        public int GamesPlayed { get; private set; }
+
+        private int points;
+        public int Points
+        {
+            get { return (Wins * 3) + Draws; }
+            set { points = value; }
+        }
+
+        public Team() { }
+
+        public Team(string name)
+        {
+            TeamName = name;
+        }
+
+        public string DisplayTeamTable()
+        {
+            return string.Format($"{TeamName}\t{Points}\t{Wins}\t{Draws}\t{Losses}\t{GamesPlayed}");
+           //return String.Format($"{TeamName,-15}{Points,-7}{Wins,-7}{Draws,-7}{Losses,-7}{GamesPlayed,-7}");
+        }
+    }
+}
